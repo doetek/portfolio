cb2c2f664ca49f91606ee326d77099c5cf129fb3
webguru12 <oyetundetobi1994@gmail.com>
2023-10-14 20:24:19 +0100

    version 1

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,27 +1,14 @@
-import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 import Home from "./home";
-import Login from '../Login';
+
 
 const Dashboard = () => {
 
-    const [user, setUser] = useState(null);
-    const auth = getAuth();
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if(user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        })
-    }, []);
-
+  
 
     return (
        <div>
-           {user ? <Home /> : <Login />}
+            <Home />
        </div>
     )
 }
