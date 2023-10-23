ace4b59a90e63738bb2030986b6b7070de92f7f9
webguru12 <oyetundetobi1994@gmail.com>
2023-10-23 10:50:12 +0100

    version 1

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,4 +1,90 @@
 
+// import React, { useEffect, useState } from 'react'
+// import Loader from 'react-loaders'
+// import AnimatedLetters from '../AnimatedLetters'
+// import Axios from 'axios' // Import axios
+// import './index.scss'
+
+// Axios.defaults.baseURL =
+//   process.env.REACT_APP_SERVER_DOMAIN || 'http://localhost:5000'
+
+// const Portfolio = () => {
+//   const [letterClass, setLetterClass] = useState('text-animate')
+//   const [portfolio, setPortfolio] = useState([])
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLetterClass('text-animate-hover')
+//     }, 3000)
+
+//     return () => {
+//       clearTimeout(timer)
+//     }
+//   }, [letterClass])
+
+//   useEffect(() => {
+//     getPortfolio()
+//   }, [])
+
+//   const getPortfolio = async () => {
+//     try {
+//       const response = await Axios.get('/api/portfolios') // Using axios here
+//       if (response.status === 200) {
+//         const data = response.data
+//         // console.log(data);
+//         setPortfolio(data)
+//       } else {
+//         console.error('Failed to fetch portfolio items')
+//       }
+//     } catch (error) {
+//       console.error('Error fetching portfolio items:', error)
+//     }
+//   }
+
+//   const renderPortfolio = (portfolio) => {
+//     return (
+//       <div className="images-container">
+//         {portfolio.map((port, idx) => {
+//           return (
+//             <div className="image-box" key={idx}>
+//               <img
+//                 src={port.image}
+//                 className="portfolio-image"
+//                 alt="portfolio"
+//               />
+//               <div className="content">
+//                 <p className="title">{port.name}</p>
+//                 <h4 className="description">{port.description}</h4>
+//                 <button className="btn" onClick={() => window.open(port.url)}>
+//                   View
+//                 </button>
+//               </div>
+//             </div>
+//           )
+//         })}
+//       </div>
+//     )
+//   }
+
+//   return (
+//     <>
+//       <div className="container portfolio-page">
+//         <h1 className="page-title">
+//           <AnimatedLetters
+//             letterClass={letterClass}
+//             strArray={'Portfolio'.split('')}
+//             idx={15}
+//           />
+//         </h1>
+//         <div>{renderPortfolio(portfolio)}</div>
+//       </div>
+//       <Loader type="pacman" />
+//     </>
+//   )
+// }
+
+// export default Portfolio
+
 import React, { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
@@ -43,18 +129,18 @@ const Portfolio = () => {
 
   const renderPortfolio = (portfolio) => {
     return (
-      <div className="images-container">
+      <div className="images-container" >
         {portfolio.map((port, idx) => {
           return (
-            <div className="image-box" key={idx}>
+            <div className="image-box" key={idx}  onClick={() => window.open(port.url)}>
               <img
                 src={port.image}
                 className="portfolio-image"
                 alt="portfolio"
               />
               <div className="content">
-                <p className="title">{port.name}</p>
-                <h4 className="description">{port.description}</h4>
+                <h2 className="title">{port.name}</h2> {/* Changed p to h2 for better SEO */}
+                {/* <p className="description">{port.description}</p> Changed h4 to p for semantic correctness */}
                 <button className="btn" onClick={() => window.open(port.url)}>
                   View
                 </button>
@@ -84,3 +170,4 @@ const Portfolio = () => {
 }
 
 export default Portfolio
+
